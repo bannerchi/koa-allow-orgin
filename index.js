e08ef723d7fa2068a1f21ce7c70ffc2677f606bf
ronchi <ron.chi@vidaxl.com>
2017-06-07 17:37:42 +0800

    #init

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = (env, writeList) => {
+	return async(ctx, next) => {
+		if(env === 'devlopment') {
+			ctx.set('Access-Control-Allow-Origin', '*')
+			ctx.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, CONNECT')
+			ctx.set('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, __setXHR_, Authorization, authorization')
+			ctx.set('Access-Control-Allow-Credentials', true);
+			await next()
+		} else {
+			let origin = ctx.headers.origin === undefined ? null : ctx.headers.origin;
+			if(origin === null) {
+				await next();
+			} else {
+				if(JSON.stringify(writeList).indexOf(origin) !== -1) {
+					ctx.set('Access-Control-Allow-Origin', origin);
+					ctx.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, CONNECT');
+					ctx.set('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, __setXHR_, Authorization, authorization');
+					ctx.set('Access-Control-Allow-Credentials', true);
+				}
+				await next();
+			}
+		}
+	}
+}
